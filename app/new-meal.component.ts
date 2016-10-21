5622e49ef36de9f344c3ac98e73091dd035e1f3a
Jena SanCartier <jenasancartier@gmail.com>
2016-10-21 09:49:18 -0700

    add new-meal component

---
+++ app/new-meal.component.ts
@@ -0,0 +1,13 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Meal } from './meal.model';
+
+@Component({
+  selector: 'new-meal',
+  template: `
+    <h3>Blank template</h3>
+  `
+})
+
+export class NewMealComponent {
+
+}
