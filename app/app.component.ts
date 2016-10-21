c500144930a43e67c4c374ef98260f0240468d4c
Jena SanCartier <jenasancartier@gmail.com>
2016-10-21 09:02:15 -0700

    add edit button for each task and provide done button so user can finish editing

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,19 +5,25 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
     <h1>Meal Tracker</h1>
-    <h3 (click)="showDetails(currentMeal)" *ngFor="let currentMeal of meals">{{currentMeal.name}}</h3>
-    <h1>Edit Meal</h1>
-    <div>
-      <label>Enter Meal Name:</label>
-      <input [(ngModel)]="selectedMeal.name">
+    <div *ngFor="let currentMeal of meals">
+      <h3>{{currentMeal.name}}</h3>
+      <button (click)="showDetails(currentMeal)">Edit</button>
     </div>
-    <div>
-      <label>Enter Calories:</label>
-      <input [(ngModel)]="selectedMeal.calories">
-    </div>
-    <div>
-      <label>Enter Details:</label>
-      <input [(ngModel)]="selectedMeal.details">
+    <div *ngIf="selectedMeal">
+      <h1>Edit Meal</h1>
+      <div>
+        <label>Enter Meal Name:</label>
+        <input [(ngModel)]="selectedMeal.name">
+      </div>
+      <div>
+        <label>Enter Calories:</label>
+        <input [(ngModel)]="selectedMeal.calories">
+      </div>
+      <div>
+        <label>Enter Details:</label>
+        <input [(ngModel)]="selectedMeal.details">
+        <button (click)="finishedEditing()">Done</button>
+      </div>
     </div>
   </div>
   `
@@ -26,12 +32,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public meals: Meal[] = [
     new Meal("Oatmeal", 250, "Apple Cinnamon"),
-    new Meal("Spring Rolls", 240, "I hate two"),
+    new Meal("Spring Rolls", 240, "I ate two"),
     new Meal("Tacos", 480, "No dairy")
   ];
-  selectedMeal: Meal = this.meals[0];
+  selectedMeal: Meal = null;
   showDetails(clickedMeal: Meal) {
     this.selectedMeal = clickedMeal;
+  }
+  finishedEditing() {
+    this.selectedMeal = null;
   }
 }
 
